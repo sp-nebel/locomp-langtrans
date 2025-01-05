ed72971eae8eff79bab12355279b1acdc4b01648
Sascha Nebel <spnebel@pm.me>
2025-01-05 02:30:47 +0100

    Add Autoconf to dims

--- print_model_dims.py
+++ print_model_dims.py
@@ -1,11 +1,15 @@
-from transformers import AutoModelForCausalLM
+from transformers import AutoConfig, AutoModelForCausalLM
 from torchinfo import summary
 
 llama_1b = "meta-llama/Llama-3.2-1B-Instruct"
 llama_3b = "meta-llama/Llama-3.2-3B-Instruct"
 
-model = AutoModelForCausalLM.from_pretrained(llama_1b)
+config = AutoConfig.from_pretrained(llama_1b)
 with open('model_dims.txt', 'w') as f:
+    f.write(str(config))
+    f.write('\n')
+model = AutoModelForCausalLM.from_pretrained(llama_1b)
+with open('model_dims.txt', 'a') as f:
     f.write(str(summary(model, verbose=1)))
     f.write(str(summary(model, verbose=2)))
     f.write('\n')
