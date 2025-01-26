a23fe696a28482bf30b0049b7bd76d6405d72b4f
spnebele <spnebel@pm.me>
2025-01-26 22:03:25 +0100

    Refine dataset loading in training script to specify train and test splits for XNLI

--- xnli_lora_training.py
+++ xnli_lora_training.py
@@ -49,7 +49,7 @@ training_args = TrainingArguments(
 )
 
 def prepare_tokenized_xnlis(tokenizer):
-    xnli_train = load_dataset('xnli', 'en', split='train+test', streaming=False)
+    xnli_train = load_dataset('xnli', 'en', split='train[:392702]+test[:5010]', streaming=False)
     xnli_train = preprocess_dataset(xnli_train, tokenizer)
     return xnli_train.train_test_split(train_size=392702, shuffle=False)
 
