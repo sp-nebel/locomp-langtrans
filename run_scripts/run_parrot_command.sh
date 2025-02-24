664b40c32af0b902a41f9b10a5c49c88e075d70d
Sascha Nebel <spnebel@pm.me>
2025-02-24 18:56:16 +0100

    Add LoRA configuration, update requirements, and create run scripts for training

---
+++ run_scripts/run_parrot_command.sh
@@ -0,0 +1,38 @@
+
+
+train_path=src/run_clm_lora.py
+model_path=meta-llama/Llama-3.2-3B-Instruct
+model_save=output/models/parrot-hint-lora-7b
+
+# HOST_NUM will be 1
+torchrun --nnodes $HOST_NUM --node_rank $INDEX --nproc_per_node 1 \
+    ${train_path} \
+    --model_name_or_path ${model_path} \
+    --train_file data/data_parrot_hf.json \
+    --use_lora True \
+    --lora_config train/lora_config.json \
+    --preprocessing_num_workers 16 \
+    --dataloader_num_workers 8 \
+    --dataloader_pin_memory True \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --num_train_epochs 1.5 \
+    --save_strategy "steps" \
+    --save_steps 500 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 10 \
+    --block_size 512 \
+    --do_train \
+    --evaluation_strategy "no" \
+    --validation_split_percentage 0 \
+    --fp16 True \
+    --fp16_full_eval True \
+    --ddp_timeout 3600 \
+    --seed 1 \
+    --gradient_checkpointing True \
+    --output_dir ${model_save}
---
+++ run_scripts/run_parrot_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=dev_gpu_4
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=00:20:00
+#SBATCH --mem=8gb
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=usxcp@student.kit.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=parrot_job
+#SBATCH --output=../parrot_job.out
+
+module load devel/miniconda
+conda activate llama_test_env
+pip install -r requirements.txt
+source run_scripts/run_parrot_command.sh
+conda deactivate
